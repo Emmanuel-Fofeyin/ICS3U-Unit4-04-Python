cc24bb32411526ab6ceb4fdf06d9909ed456c17f
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-11-22 16:12:12 +0000

    facts

--- ICS3U-Unit4-04.py
+++ ICS3U-Unit4-04.py
@@ -12,18 +12,21 @@ def main():
 
     # input and variables
     random_number = random.randint(0, 9)
-    guessed_number_as_string = input("Enter a number between 0 and 9: ")
 
     # process and output
     print("")
     try:
-        guessed_number_as_number = int(guessed_number_as_string)
         while True:
-            if int(guessed_number_as_number) == random_number:
+            guessed_number_as_string = input("Enter a number between 0 and 9: ")
+            guessed_number_as_number = int(guessed_number_as_string)
+            if guessed_number_as_number == random_number:
                 print("\nYou guessed correctly!")
-            elif int(guessed_number_as_number) != random_number:
-                print("You guessed incorrectly, the number was {}.".format(random_number))
                 break
+            else:
+                print(
+                    "You guessed incorrectly, the number was {}.".format(random_number)
+                )
+                
     except ValueError:
         print("\nNot good, {} is not an integer.".format(guessed_number_as_string))
     finally:
